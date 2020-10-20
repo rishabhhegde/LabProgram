5c290fc00adf864f8f35b1d756496e98f1280c64
rishabhhegde <rishabhhegde8@gmail.com>
2020-10-20 15:02:12 +0530

    prog 6

---
+++ prog6/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Prog 6</title>
+</head>
+<body>
+ <h1>Hello Everyone</h1>
+ <h2>x=50 and y=80</h2>
+  <?php
+    $x=50;
+    $y=80;
+    $res=$x+$y;
+    echo "<h2>Result = $res</h2>";
+  ?>
+</body>
+</html>
+
+
+

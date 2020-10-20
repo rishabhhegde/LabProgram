3bf2e915856301f6062d6da9f5bd72f217e77a8b
rishabhhegde <rishabhhegde8@gmail.com>
2020-10-20 16:11:31 +0530

    prog 6

--- prog6/index.php
+++ prog6/index.php
@@ -1,6 +1,11 @@
 <html>
 <head>
     <title>Prog 6</title>
+    <style>
+        h1,h2{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
  <h1>Number of visited who visited your webpage</h1>
@@ -9,7 +14,10 @@
     $count=fread($handle,10);
     fclose($handle);
     $count=intval($count);
-    $count+=1;
+    date_default_timezone_set("Asia/Kolkata");
+    $curTime=date("H");
+    if($curTime>=9 && $curTime<17)
+        $count+=1;
     echo "<h2>Number of times the website was visited $count</h2>";
     $handle=fopen("count.txt","w");
     fwrite($handle,$count);
